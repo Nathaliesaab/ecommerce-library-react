ee5836be5f8e2c78e0fa441b11e080d70952967a
Nathaliesaab <84041880+Nathaliesaab@users.noreply.github.com>
2022-09-05 10:54:54 +0300

    added loading state and nav fixes

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,21 +1,26 @@
 import React from "react";
-import UndrawBooks from '../assets/Undraw_Books.svg'
 import { Link } from "react-router-dom";
+import UndrawBooks from "../assets/Undraw_Books.svg";
+
 export const Landing = () => {
     return (
-        <header>
-            <div className="header__container">
-                <div className="header__description">
-                    <h1>America's most awarded online library platform</h1>
-                    <h2>Find your dream book with <span className="purple">Library</span></h2>
-                    <Link to="/books">
-                        <button className="btn">Browse books</button>
-                    </Link>
+        <section id="landing">
+            <header>
+                <div className="header__container">
+                    <div className="header__description">
+                        <h1>Australias most awarded online library platfrom</h1>
+                        <h2>
+                            Find your dream book with <span className="purple">Library</span>
+                        </h2>
+                        <Link to="#features">
+                            <button className="btn">Browse books</button>
+                        </Link>
+                    </div>
+                    <figure className="header__img--wrapper">
+                        <img src={UndrawBooks} alt="" />
+                    </figure>
                 </div>
-                <figure className="header__img--wrapper">
-                    <img src={UndrawBooks} alt="" />
-                </figure>
-            </div>
-        </header>
+            </header>
+        </section>
     );
-}
+};
